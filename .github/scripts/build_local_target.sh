967b08b9d153bca7bb519de74e2b5445333fd532
Eryk Szpotanski <eszpotanski@antmicro.com>
2024-04-29 16:01:35 +0200

    CI: Modify scripts for new _scripts and _make targets
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- .github/scripts/build_local_target.sh
+++ .github/scripts/build_local_target.sh
@@ -3,6 +3,7 @@
 set -e
 
 target_name=${TARGET:-"tag_array_64x184"}
+flow=${FLOW:-"local_make"}
 if [[ -z "$STAGES" ]]; then
   if [[ "$target_name" == L1MetadataArray_* ]]; then
     STAGES=("synth_sdc" "synth" "floorplan" "place" "cts" "grt" "generate_abstract")
@@ -18,13 +19,13 @@ for stage in ${STAGES[@]}
 do
   if [[ -z $SKIP_BUILD ]] ; then
     echo "query make script target"
-    bazel query ${target_name}_${stage}_make
-    bazel query ${target_name}_${stage}_make --output=build
+    bazel query ${target_name}_${stage}_scripts
+    bazel query ${target_name}_${stage}_scripts --output=build
     echo "build make script"
-    bazel build --subcommands --verbose_failures --sandbox_debug ${target_name}_${stage}_make
+    bazel build --subcommands --verbose_failures --sandbox_debug ${target_name}_${stage}_scripts
   fi
   if [[ -z $SKIP_RUN ]] ; then
     echo "run make script"
-    ./bazel-bin/${target_name}_${stage}_make $(if [[ "$stage" != "memory" ]] ; then echo "bazel-" ; fi)${stage}
+    ./bazel-bin/${target_name}_${stage}_${flow} $(if [[ "$stage" != "memory" ]] ; then echo "bazel-" ; fi)${stage}
   fi
 done
