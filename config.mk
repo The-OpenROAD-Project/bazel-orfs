57ed9d98f91fe333e8143465c4fd84143609e31f
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-05-22 16:00:46 +0200

    cleanup: delete unused file
    Co-authored-by: Øystein Hovind <oystein@ascenium.com>
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- config.mk
+++
@@ -1,9 +0,0 @@
-# Hmmm.... PLATFORM can't be passed on the command line as "make PLATFORM=asap7", because
-# include of the platform config.mk doesn't see PLATFORM=aspa7.
-export PLATFORM=asap7
-
-export WORK_HOME_READ?=$(WORK_HOME)
-
-# $(MAKE_PATTERN) stores the path to file with make patterns
-# that will be called in the given flow.
-include $(MAKE_PATTERN)
