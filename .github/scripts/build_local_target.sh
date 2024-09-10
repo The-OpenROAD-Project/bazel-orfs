ef9daba9f89ed6e166d4df3521ce149d1aa0d18e
Mateusz Leonowicz <mleonowicz@antmicro.com>
2024-09-10 13:59:42 +0200

    CI: Update build_local_target script
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

--- .github/scripts/build_local_target.sh
+++ .github/scripts/build_local_target.sh
@@ -3,29 +3,49 @@
 set -e
 
 target_name=${TARGET:-"tag_array_64x184"}
-flow=${FLOW:-"local_make"}
 if [[ -z "$STAGES" ]]; then
-  if [[ "$target_name" == L1MetadataArray_* ]]; then
-    STAGES=("synth_sdc" "synth" "floorplan" "place" "generate_abstract")
-  else
-    STAGES=("synth_sdc" "synth" "floorplan" "generate_abstract")
-  fi
+  STAGES=("synth" "floorplan" "place" "cts" "grt" "route")
 else
   eval "STAGES=($STAGES)"
 fi
 
-echo "Build tag_array_64x184 macro"
-for stage in ${STAGES[@]}
+echo "Build ${target_name} macro"
+for stage in "${STAGES[@]}"
 do
   if [[ -z $SKIP_BUILD ]] ; then
-    echo "query make script target"
-    bazel query ${target_name}_${stage}_scripts
-    bazel query ${target_name}_${stage}_scripts --output=build
-    echo "build make script"
-    bazel build --subcommands --verbose_failures --sandbox_debug ${target_name}_${stage}_scripts
+    echo "[${target_name}] ${stage}: Query dependency target"
+    bazel query "${target_name}_${stage}_deps"
+    bazel query "${target_name}_${stage}_deps" --output=build
+    echo "[${target_name}] ${stage}: Build dependency"
+    bazel run --subcommands --verbose_failures --sandbox_debug "${target_name}_${stage}_deps" -- "$(pwd)/build"
   fi
   if [[ -z $SKIP_RUN ]] ; then
-    echo "run make script"
-    ./bazel-bin/${target_name}_${stage}_${flow} bazel-${stage}
+    stages=()
+    if [[ $stage == "synth" ]]; then
+        stages+=("do-yosys-canonicalize")
+        stages+=("do-yosys-keep-hierarchy")
+        stages+=("do-yosys")
+        stages+=("do-synth")
+    elif [[ $stage == "grt" ]]; then
+        stages+=("do-5_1_grt")
+    elif [[ $stage == "route" ]]; then
+        stages+=("do-5_2_fillcell")
+        stages+=("do-5_3_route")
+    else
+        stages+=("do-${stage}")
+    fi
+    for local_stage in "${stages[@]}"
+    do
+        echo "[${target_name}] ${local_stage}: Run make script"
+        build/make "${local_stage}"
+    done
   fi
 done
+
+if [[ -z $SKIP_BUILD && -z $SKIP_ABSTRACT ]]; then
+    echo "query abstract target"
+    bazel query "${target_name}_generate_abstract"
+    bazel query "${target_name}_generate_abstract" --output=build
+    echo "build abstract"
+    bazel build --subcommands --verbose_failures --sandbox_debug "${target_name}_generate_abstract"
+fi
