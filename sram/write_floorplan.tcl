02027dc546b21715c9d2c9de794997308ae89e0e
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-10-02 06:46:05 +0200

    sram: fix floorplan.def
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- sram/write_floorplan.tcl
+++ sram/write_floorplan.tcl
@@ -1,4 +1,14 @@
-source $::env(SCRIPTS_DIR)/floorplan.tcl
+# Make a minimal floorplan.def that we can read in using
+# FLOORPLAN_DEF
+source $::env(SCRIPTS_DIR)/load.tcl
+load_design 1_synth.v 1_synth.sdc
+
+set additional_args ""
+append_env_var additional_args ADDITIONAL_SITES -additional_sites 1
+initialize_floorplan -die_area $::env(DIE_AREA) \
+                        -core_area $::env(CORE_AREA) \
+                        -site $::env(PLACE_SITE) \
+                        {*}$additional_args
 
 set f [file join $::env(WORK_HOME) "floorplan.def"]
 write_def $f
