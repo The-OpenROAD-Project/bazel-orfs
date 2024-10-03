394c0eb185d58fff64c2d34cb53ff36e6e1c84ae
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-10-03 15:16:56 +0200

    sram: use extra_configs
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- sram/write_floorplan.tcl
+++ sram/write_floorplan.tcl
@@ -1,4 +1,15 @@
 source $::env(SCRIPTS_DIR)/floorplan.tcl
 
-set f [file join $::env(WORK_HOME) "floorplan.def"]
-write_def $f
+set db [::ord::get_db]
+set dbu_per_uu [expr double([[$db getTech] getDbUnitsPerMicron])]
+set block [[$db getChip] getBlock]
+set die_bbox [$block getDieArea]
+set core_bbox [$block getCoreArea]
+set scale [expr 1 / $dbu_per_uu]
+
+set file [open [file join $::env(WORK_HOME) "floorplan.config"] w]
+puts $file "export DIE_AREA=0 0 [expr $scale*[$die_bbox xMax]] [expr $scale*[$die_bbox yMax]]"
+puts $file "export CORE_AREA=[expr $scale*[$core_bbox xMin]] [expr $scale*[$core_bbox yMin]]\
+[expr $scale*([$die_bbox xMax] - ([$die_bbox xMax] - [$core_bbox xMax]))]\
+[expr $scale*([$die_bbox yMax] - ([$die_bbox yMax] - [$core_bbox yMax]))]"
+close $file
