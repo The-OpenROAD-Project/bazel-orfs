4014422a3f871653fb193ca537d76360a1165176
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-10-21 21:56:11 +0200

    debug: test
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- .github/scripts/smoketests.sh
+++ .github/scripts/smoketests.sh
@@ -2,7 +2,8 @@
 set -ex
 rm -rf ./tmp
 bazel run //sram:sdq_17x64_mock-naja_floorplan_deps $(pwd)/tmp
-ls -l $(pwd)/tmp/sram
+ls -l tmp/sram/results/asap7/sdq_17x64/mock-naja/
+head -n 5 tmp/sram/results/asap7/sdq_17x64/mock-naja/1_synth.v
 grep naja tmp/sram/results/asap7/sdq_17x64/mock-naja/1_synth.v
 
 bazel build lb_32x128_shared_synth_floorplan wns_report //sram:sdq_17x64_mock-naja_floorplan_deps //sram:mock-naja
