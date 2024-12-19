279dad9dbb945c179d9ae065efac2c576356aad1
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-12-19 12:03:55 +0100

    fix: lb_32x128_top_final now works again
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- .github/scripts/smoketests.sh
+++ .github/scripts/smoketests.sh
@@ -14,7 +14,8 @@ bazel build \
  lb_32x128_wns_report \
  //sram:sdq_17x64_mock-naja_floorplan_deps \
  //sram:mock-naja \
- sta
+ sta \
+ lb_32x128_top_final
 grep naja bazel-bin/sram/mock-naja.v
 grep -q naja bazel-bin/sram/results/asap7/sdq_17x64/mock-naja/1_synth.v && false || true
 (bazel build //sram:sdq_17x64_naja-error_floorplan 2>&1 || true) | grep "syntax error"
