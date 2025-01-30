c2499fa7fa56326d5e9651666f8458a84323f783
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-01-30 20:34:56 +0100

    mock area: scale die area, keep die to core margin
    fixes problems with not enough space for PDN
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- mock_area.tcl
+++ mock_area.tcl
@@ -13,10 +13,20 @@ set die_bbox [$block getDieArea]
 set core_bbox [$block getCoreArea]
 set scale [expr $::env(MOCK_AREA) / $dbu_per_uu]
 
+proc area_um {bbox} {
+  global dbu_per_uu
+  return "[expr [$bbox xMin] / $dbu_per_uu] [expr [$bbox yMin] / $dbu_per_uu] [expr [$bbox xMax] / $dbu_per_uu] [expr [$bbox yMax] / $dbu_per_uu]"
+}
+
+puts "DIE_AREA: [area_um $die_bbox]"
+puts "CORE_AREA: [area_um $core_bbox]"
+
 set file [open $out_file w]
 tee $file "export DIE_AREA=0 0 [expr $scale*[$die_bbox xMax]] [expr $scale*[$die_bbox yMax]]"
-tee $file "export CORE_AREA=[expr $scale*[$core_bbox xMin]] [expr $scale*[$core_bbox yMin]]\
-[expr $scale*([$die_bbox xMax] - ([$die_bbox xMax] - [$core_bbox xMax]))]\
-[expr $scale*([$die_bbox yMax] - ([$die_bbox yMax] - [$core_bbox yMax]))]"
+# keep same margin between DIE_ARE and CORE_AREA
+tee $file "export CORE_AREA=[expr ([$core_bbox xMin] - [$die_bbox xMin]) / $dbu_per_uu] \
+ [expr ([$core_bbox yMin] - [$die_bbox yMin]) / $dbu_per_uu] \
+ [expr $scale*[$die_bbox xMax] - ([$die_bbox xMax] - [$core_bbox xMax]) / $dbu_per_uu ] \
+ [expr $scale*[$die_bbox yMax] - ([$die_bbox yMax] - [$core_bbox yMax]) / $dbu_per_uu]"
 tee $file "export CORE_UTILIZATION="
 close $file
