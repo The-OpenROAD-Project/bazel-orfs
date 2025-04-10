8da5941d961ac40f9397f79599362960aa085b67
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-04-10 11:03:19 +0200

    sta: fix deprecation warning for -group_count argument
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- report-wns.tcl
+++ report-wns.tcl
@@ -1,7 +1,7 @@
 source $::env(SCRIPTS_DIR)/load.tcl
 load_design 4_cts.odb 4_cts.sdc
 
-set paths [find_timing_paths -path_group reg2reg -sort_by_slack -group_count 1]
+set paths [find_timing_paths -path_group reg2reg -sort_by_slack -group_path_count 1]
 set path [lindex $paths 0]
 set slack [get_property $path slack]
 
--- sweep-wns.tcl
+++ sweep-wns.tcl
@@ -3,7 +3,7 @@
 # ODB_FILE=results/nangate45/gcd/base/2_floorplan.odb make run RUN_SCRIPT=~/megaboom/report-wns.tcl
 source $::env(SCRIPTS_DIR)/open.tcl
 
-set paths [find_timing_paths -path_group reg2reg -sort_by_slack -group_count 1]
+set paths [find_timing_paths -path_group reg2reg -sort_by_slack -group_path_count 1]
 set path [lindex $paths 0]
 set slack [get_property $path slack]
 
