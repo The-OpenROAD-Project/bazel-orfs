ca614657a795785bd4b472e406fc1d583e0620a2
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-06-13 19:37:14 +0200

    smoketest: keep going and output errors
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- .github/scripts/smoketests.sh
+++ .github/scripts/smoketests.sh
@@ -8,7 +8,11 @@ bazelisk query //:* | grep -q -v lb_32x128_3_place
 echo This target should exist
 bazelisk query //:* | grep -q -v lb_32x128_4_synth
 
-bazelisk test ... --profile=build.profile
+bazelisk test ... \
+   --keep_going \
+   --test_output=errors \
+   --profile=build.profile
+
 grep naja bazel-bin/sram/mock-naja.v
 grep -q naja bazel-bin/sram/results/asap7/sdq_17x64/mock-naja/1_synth.v && false || true
 (bazelisk build //sram:sdq_17x64_naja-error_floorplan 2>&1 || true) | grep "syntax error"
