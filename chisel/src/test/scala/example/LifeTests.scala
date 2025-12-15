432bd64ce61e1a610050e5723934d1acd8c2e69e
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-12-15 18:25:15 +0100

    blah

--- chisel/src/test/scala/example/LifeTests.scala
+++ chisel/src/test/scala/example/LifeTests.scala
@@ -1,6 +1,7 @@
 package example
 
 import chisel3._
+import chisel3.simulator.HasSimulator.simulators
 import chisel3.simulator.scalatest.ChiselSim
 import chisel3.simulator.stimulus.{RunUntilFinished, RunUntilSuccess}
 import chisel3.util.Counter
@@ -9,7 +10,10 @@ import org.scalatest.funspec.AnyFunSpec
 class LifeTests extends AnyFunSpec with ChiselSim {
   describe("Baz") {
     it("counts to 42") {
-      simulate(new LifeUniverse(42)) { foo =>
+      implicit val verilator = simulators
+      .verilator(verilatorSettings =
+        svsim.verilator.Backend.CompilationSettings.default.withDisableFatalExitOnWarnings(true))
+      simulate(new LifeUniverse(42), Array(), Array()) { foo =>
         foo.enable.poke(true.B)
         foo.clock.step(1)
         foo.out.expect(1.U)
